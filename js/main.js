94fb4e855ec8765afd01cc67a10fcc83c2a14ef7
jakeremnant <jake.remnant@gmail.com>
2024-11-08 06:53:54 -0500

    A lot of updates

--- js/main.js
+++ js/main.js
@@ -1,15 +1,5 @@
-let currentSlide = 0;
-const slides = document.querySelectorAll(".photoitem");
-
-function showSlide(index) {
-  slides.forEach(slide => slide.classList.remove("active"));
-  slides[index].classList.add("active");
+function toggleMenu() {
+  console.log("toggleMenu function triggered");
+  const navbar = document.getElementById('navbar').querySelector('ul');
+  navbar.classList.toggle('show');
 }
-
-function changeSlide(direction) {
-  currentSlide = (currentSlide + direction + slides.length) % slides.length;
-  showSlide(currentSlide);
-}
-
-// Initialize the first slide
-showSlide(currentSlide);
